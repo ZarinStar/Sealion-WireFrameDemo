aadef27f0e271e152709bfba05e6e7b275e7ec4f
Dar'ya <ZarinStar@users.noreply.github.com>
2017-11-02 12:19:23 -0400

    Create index.php
    For log in page

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html>
+
+<HEAD> <title>Home</title> 
+</HEAD>
+
+<!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+
+<body>
+<!-- Horizontal bar-->
+<?php include 'header.php'; ?>
+
+<p> home page (test) </p>
+
+<br>
+</body>
+
+<?php include('footer.html'); ?>
+
+</html
